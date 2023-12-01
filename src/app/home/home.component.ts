a9796ee03a094ab5b4565234f9c719fe95644fe4
Pablo <pnunfe@gmail.com>
2023-12-01 10:17:24 +0100

    Add routes for home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    standalone: true,
+    selector: 'app-home',
+    template: `
+    <p>Hello World</p>`
+})
+
+export default class HomeComponent {
+}
