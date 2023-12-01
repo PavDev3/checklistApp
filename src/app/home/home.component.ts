7f4b998c2450dcfe226f863c64ad0aa0cb9419ef
Pablo <pnunfe@gmail.com>
2023-12-01 10:48:43 +0100

    Add checklist functionality to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { ModalComponent } from "../shared/ui/modal.component";
+import { Checklist } from "../shared/interafaces/checklist";
 
 @Component ({
     standalone: true,
     selector: 'app-home',
+    imports: [ModalComponent],
     template: `
-    <p>Hello World</p>`
+    <header>
+        <h1>Checklist</h1>
+        <button (click)="checklistBeingEdited.set({})">Add Checklist</button>
+    </header>
+    
+    <app-modal [isOpen]="!!checklistBeingEdited()">
+        <ng-template> No puedes verlo aun</ng-template>
+    </app-modal>`
 })
 
 export default class HomeComponent {
+    checklistBeingEdited = signal<Partial<Checklist> | null>(null);
 }
---
+++ src/app/shared/interafaces/checklist.ts
@@ -0,0 +1,4 @@
+export interface Checklist {
+    id: string;
+    tittle: string;  
+}
